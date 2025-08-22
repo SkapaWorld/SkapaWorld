cfc5715daf518c8925e6f51c6dff670dc400d48e
GnomKannibal <gkannibal@bk.ru>
2025-08-23 00:45:37 +0300

    NewBuild

--- index.js
+++ index.js
@@ -1,70 +1,52 @@
-// Playdeck API wrapper
+window.PlaydeckAPI = {
+  loading: (value = 0) =>
+    parent.postMessage({ playdeck: { method: "loading", value } }, "*"),
 
-// слушаем события от Playdeck
+  setScore: (value, isForce = false) =>
+    parent.postMessage({ playdeck: { method: "setScore", value, isForce } }, "*"),
+
+  getScore: () =>
+    parent.postMessage({ playdeck: { method: "getScore" } }, "*"),
+
+  gameEnd: () =>
+    parent.postMessage({ playdeck: { method: "gameEnd" } }, "*"),
+
+  getUser: () =>
+    parent.postMessage({ playdeck: { method: "getUser" } }, "*"),
+
+  getUserLocale: () =>
+    parent.postMessage({ playdeck: { method: "getUserLocale" } }, "*"),
+
+  setData: (key, value) =>
+    parent.postMessage({ playdeck: { method: "setData", key, value } }, "*"),
+
+  getData: (key) =>
+    parent.postMessage({ playdeck: { method: "getData", key } }, "*"),
+
+  openInvoice: (params) =>
+    parent.postMessage({ playdeck: { method: "openInvoice", value: params } }, "*"),
+};
+
 window.addEventListener("message", ({ data }) => {
   const pdData = data?.playdeck;
   if (!pdData) return;
 
-  console.log("[Playdeck ▶️]:", pdData.method, pdData.value);
+  console.log("[Playdeck message]:", pdData.method, pdData.value);
 
-  switch (pdData.method) {
-    case "isOpen":
-      window.playdeckIsOpen = pdData.value;
-      break;
-    case "getUser":
-      window.playdeckUser = pdData.value;
-      break;
-    case "getUserLocale":
-      window.playdeckUserLocale = pdData.value;
-      break;
-    case "getScore":
-      window.playdeckScore = pdData.value;
-      break;
-    case "getData":
-      window.playdeckData = pdData.value;
-      break;
-    case "play":
-      console.log("▶️ Запуск игры (событие Play)");
-      if (window.unityInstance) {
-        // Вызываем метод Unity, если нужно (GameManager.StartGame)
-        // window.unityInstance.SendMessage("GameManager", "StartGame");
-      }
-      break;
-    case "invoiceClosed":
-      console.log("💰 Платёж закрыт:", pdData.value);
-      break;
-    case "rewardedAd":
-      console.log("🎁 Награда за рекламу");
-      break;
+  if (pdData.method === "getUser") {
+    window.playdeckUser = pdData.value;
+    console.log("User:", pdData.value);
+  }
+
+  if (pdData.method === "getUserLocale") {
+    window.playdeckUserLocale = pdData.value;
+  }
+
+  if (pdData.method === "getScore") {
+    window.playdeckScore = pdData.value;
   }
 });
 
-const parentWin = window.parent !== window ? window.parent : null;
-const post = (payload) => parentWin?.postMessage({ playdeck: payload }, "*");
-
-const loading = (value) => post({ method: "loading", value });
-const getPlaydeckState = () => post({ method: "getPlaydeckState" });
-const getScore = () => post({ method: "getScore" });
-const setScore = (value, isForce = false) =>
-  post({ method: "setScore", value, isForce });
-const getData = (key) => post({ method: "getData", key });
-const setData = (key, value) => post({ method: "setData", key, value });
-const getUserLocale = () => post({ method: "getUserLocale" });
-const getUser = () => post({ method: "getUser" });
-const gameEnd = () => post({ method: "gameEnd" });
-const showAd = () => post({ method: "showAd" });
-const requestPayment = (amount, description, externalId) =>
-  post({ method: "requestPayment", value: { amount, description, externalId } });
-
-window.PlaydeckAPI = {
-  loading,
-  getPlaydeckState,
-  getScore,
-  setScore,
-  getData,
-  setData,
-  getUserLocale,
-  getUser,
-  gameEnd,
-  showAd,
-  requestPayment,
+if (window.Telegram?.WebApp) {
+  window.Telegram.WebApp.ready();
+}
